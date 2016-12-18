ef5e18234a37ced1373063fda63b2a0f18453b08
Youngju Kim <dawkiny@gmail.com>
2016-12-18 13:41:54 +0900

    Update start_bash.sh

--- scripts/hadoop_ecosystem/start_bash.sh
+++ scripts/hadoop_ecosystem/start_bash.sh
@@ -1,15 +1,18 @@
+#--------------Start all ----------------#
+
+
 # Zookeeper
-ssh dawkiny@hd0m2 zkServer.sh start
-ssh dawkiny@hd0s1 zkServer.sh start
-ssh dawkiny@hd0s2 zkServer.sh start
-ssh dawkiny@hd0s3 zkServer.sh start
-ssh dawkiny@hd0s4 zkServer.sh start
+zkServer.sh start
+ssh dawkiny@hd0m2 <(zkServer.sh start)
+ssh dawkiny@hd0s1 <(zkServer.sh start)
+ssh dawkiny@hd0s2 <(zkServer.sh start)
+ssh dawkiny@hd0s3 <(zkServer.sh start)
+ssh dawkiny@hd0s4 <(zkServer.sh start)
 
 # Kafka
-source ~/.bashrc
-ssh dawkiny@hd0m2 kafka-server-start.sh server.properties
-ssh dawkiny@hd0s1 kafka-server-start.sh server.properties
-ssh dawkiny@hd0s2 kafka-server-start.sh server.properties
-ssh dawkiny@hd0s3 kafka-server-start.sh server.properties
-ssh dawkiny@hd0s4 kafka-server-start.sh server.properties
-
+kafka-server-start.sh server.properties
+ssh dawkiny@hd0m2 <(kafka-server-start.sh server.properties)
+ssh dawkiny@hd0s1 <(kafka-server-start.sh server.properties)
+ssh dawkiny@hd0s2 <(kafka-server-start.sh server.properties)
+ssh dawkiny@hd0s3 <(kafka-server-start.sh server.properties)
+ssh dawkiny@hd0s4 <(kafka-server-start.sh server.properties)
