bbea3d5f6ef67ea41cd70e13509102b75b98f598
Youngju Kim <dawkiny@gmail.com>
2016-12-18 13:12:32 +0900

    Update push_bash.sh

--- scripts/hadoop_ecosystem/push_bash.sh
+++ scripts/hadoop_ecosystem/push_bash.sh
@@ -5,6 +5,15 @@ scp -r ~/.bashrc dawkiny@hd0s2:~/
 scp -r ~/.bashrc dawkiny@hd0s3:~/
 scp -r ~/.bashrc dawkiny@hd0s4:~/
 
+## Re-run .bashrc
+source ~/.bashrc
+ssh dawkiny@hd0m2 source ~/.bashrc
+ssh dawkiny@hd0m2 source ~/.bashrc
+ssh dawkiny@hd0m2 source ~/.bashrc
+ssh dawkiny@hd0m2 source ~/.bashrc
+ssh dawkiny@hd0m2 source ~/.bashrc
+
+
 # Push Haddop Ecosystem Folder
 scp -r /usr/local/hadoop_eco dawkiny@hd0m2:/usr/local/
 scp -r /usr/local/hadoop_eco dawkiny@hd0s1:/usr/local/
