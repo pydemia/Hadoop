7108e49c9de4ed0eea713fb7d78abadea05e666b
Youngju Kim <dawkiny@gmail.com>
2016-12-19 00:42:29 +0900

    Update push_bash.sh

--- scripts/hadoop_ecosystem/push_bash.sh
+++ scripts/hadoop_ecosystem/push_bash.sh
@@ -21,18 +21,3 @@ scp -r /usr/local/hadoop_eco dawkiny@hd0s2:/usr/local/
 scp -r /usr/local/hadoop_eco dawkiny@hd0s3:/usr/local/
 scp -r /usr/local/hadoop_eco dawkiny@hd0s4:/usr/local/
 
-# Push Unique Variable for Each Node
-
-## Zookeeper
-scp -r /usr/local/hadoop_eco/node_variables/hd0m2/myid dawkiny@hd0m2:/usr/local/hadoop_eco/zookeeper/data/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s1/myid dawkiny@hd0s1:/usr/local/hadoop_eco/zookeeper/data/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s2/myid dawkiny@hd0s2:/usr/local/hadoop_eco/zookeeper/data/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s3/myid dawkiny@hd0s3:/usr/local/hadoop_eco/zookeeper/data/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s4/myid dawkiny@hd0s4:/usr/local/hadoop_eco/zookeeper/data/
-
-## Kafka
-scp -r /usr/local/hadoop_eco/node_variables/hd0m2/server.properties dawkiny@hd0m2:/usr/local/hadoop_eco/kafka/config/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s1/server.properties dawkiny@hd0s1:/usr/local/hadoop_eco/kafka/config/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s2/server.properties dawkiny@hd0s2:/usr/local/hadoop_eco/kafka/config/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s3/server.properties dawkiny@hd0s3:/usr/local/hadoop_eco/kafka/config/
-scp -r /usr/local/hadoop_eco/node_variables/hd0s4/server.properties dawkiny@hd0s4:/usr/local/hadoop_eco/kafka/config/
