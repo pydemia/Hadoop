34852fa0ccc14624b379594cd5c7e27046170688
Youngju Kim <dawkiny@gmail.com>
2016-12-18 12:31:54 +0900

    Create push_bash.sh

---
+++ scripts/hadoop_ecosystem/push_bash.sh
@@ -0,0 +1,14 @@
+# Push .bashrc
+scp -r ~/.bashrc dawkiny@hd0m2:~/
+scp -r ~/.bashrc dawkiny@hd0s1:~/
+scp -r ~/.bashrc dawkiny@hd0s2:~/
+scp -r ~/.bashrc dawkiny@hd0s3:~/
+scp -r ~/.bashrc dawkiny@hd0s4:~/
+
+# Push Haddop Ecosystem Folder
+scp -r /usr/local/hadoop_eco dawkiny@hd0m2:/usr/local/
+scp -r /usr/local/hadoop_eco dawkiny@hd0s1:/usr/local/
+scp -r /usr/local/hadoop_eco dawkiny@hd0s2:/usr/local/
+scp -r /usr/local/hadoop_eco dawkiny@hd0s3:/usr/local/
+scp -r /usr/local/hadoop_eco dawkiny@hd0s4:/usr/local/
+
