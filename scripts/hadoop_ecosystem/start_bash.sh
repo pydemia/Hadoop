3aba44cf6b7b0f40db2eef954d8a615756c7e309
Youngju Kim <dawkiny@gmail.com>
2016-12-19 02:02:34 +0900

    Update start_bash.sh

--- scripts/hadoop_ecosystem/start_bash.sh
+++ scripts/hadoop_ecosystem/start_bash.sh
@@ -1,34 +1,30 @@
 #--------------Start all ----------------#
+
 # HDFS
-```sh
 start-dfs.sh
-```
+
 
 # Yarn
-```sh
 start-yarn.sh
-```
+
 
 # MR History Server
-```sh
 mr-jobhistory-daemon.sh start historyserver
-```
+
 
 
 # Zookeeper
-```sh
 zkServer.sh start
 ssh dawkiny@hd0m2 "/usr/local/hadoop_eco/zookeeper/bin/zkServer.sh start"    
 ssh dawkiny@hd0s1 "/usr/local/hadoop_eco/zookeeper/bin/zkServer.sh start" 
 ssh dawkiny@hd0s2 "/usr/local/hadoop_eco/zookeeper/bin/zkServer.sh start" 
 ssh dawkiny@hd0s3 "/usr/local/hadoop_eco/zookeeper/bin/zkServer.sh start" 
 ssh dawkiny@hd0s4 "/usr/local/hadoop_eco/zookeeper/bin/zkServer.sh start" 
-```
+
 
 # HBase
-```sh
 start-hbase.sh
-```
+
 
 # Kafka
 kafka-server-start.sh server.properties
