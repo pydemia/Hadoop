c25c869c055eb25004a027db8cca0fbea94158d2
Youngju Kim <dawkiny@gmail.com>
2016-12-21 01:17:09 +0900

    Update push_bash.sh

--- scripts/hadoop_ecosystem/push_bash.sh
+++ scripts/hadoop_ecosystem/push_bash.sh
@@ -1,23 +1,21 @@
-# Push .bashrc
-scp -r ~/.bashrc dawkiny@hd0m2:~/
-scp -r ~/.bashrc dawkiny@hd0s1:~/
-scp -r ~/.bashrc dawkiny@hd0s2:~/
-scp -r ~/.bashrc dawkiny@hd0s3:~/
-scp -r ~/.bashrc dawkiny@hd0s4:~/
+#----------------Push to Nodes---------------#
+array=(hd0m2 hd0s1 hd0s2 hd0s3 hd0s4)
 
-## Re-run .bashrc
+
 source ~/.bashrc
-ssh dawkiny@hd0m2 "source ~/.bashrc"
-ssh dawkiny@hd0s1 "source ~/.bashrc"
-ssh dawkiny@hd0s2 "source ~/.bashrc"
-ssh dawkiny@hd0s3 "source ~/.bashrc"
-ssh dawkiny@hd0s4 "source ~/.bashrc"
+
+for i in "${array[@]}"
+
+do
+        # Push .bashrc
+        scp -r ~/.bashrc dawkiny@$i:~/
+
+        ## Re-run .bashrc
+
+        ssh dawkiny@hd0m2 "source ~/.bashrc"
 
 
-# Push Haddop Ecosystem Folder
-scp -r /usr/local/hadoop_eco dawkiny@hd0m2:/usr/local/
-scp -r /usr/local/hadoop_eco dawkiny@hd0s1:/usr/local/
-scp -r /usr/local/hadoop_eco dawkiny@hd0s2:/usr/local/
-scp -r /usr/local/hadoop_eco dawkiny@hd0s3:/usr/local/
-scp -r /usr/local/hadoop_eco dawkiny@hd0s4:/usr/local/
+        # Push Haddop Ecosystem Folder
+        scp -r /usr/local/hadoop_eco dawkiny@$i:/usr/local/
 
+done
